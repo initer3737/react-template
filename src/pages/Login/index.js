b9a13322435d473931d7f9fe81174a2add1bea63
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-09-10 00:08:50 +0700

    first commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <p>login section</p>
+  )
+}
+
+export default Login
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../../assets/images/not-found.jpg';
+const NotFound = () => {
+  return (
+    <img src={Logo}/>
+     
+  )
+}
+
+export default NotFound;
