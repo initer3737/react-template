91f5e1e79f7076b03bf15ebab021e4e2a26d6708
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-10-07 02:03:54 +0700

    update friday october 07 2022 2:03 AM

--- src/components/molekuls/footer/index.tsx
+++ src/components/molekuls/footer/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
-import { Icon } from '../../atom'
-import Button from '../../atom/button'
+import LogoFooter from '../../../imgs/cat-133.gif'
+import LogoFooter2 from '../../../imgs/kitten-7157608.png'
+import { Icon, Img } from '../../atom'
 export default function Footer() {
   return (
-    <footer className="mb-auto text-light row bg-4 p-2">
-      <div className="container text-center">
-        <div className="col-3 col-md-6 col-12">
-         
+    <footer className="mb-auto text-light row bg-4 p-2 mt-2">
+        <div className="col-12">
+            <h5 className="fs-5 text-capitalize text-light text-center">
+             <Icon variant={'light'} icon={'hearts'} name={' mero mero mero!'}/> 
+            </h5>
         </div>
-        <div className="col-3 col-md-6 col-12"></div>
-        <div className="col-3 col-md-6 col-12"></div>
-        <div className="col-3 col-md-6 col-12"></div>
-      </div>
+        <div className="col-12 d-flex justify-content-between align-items-center align-self-center">
+        <Img 
+                src={LogoFooter} 
+                alt={''} 
+                srcset={''} 
+                className={'w-25'} 
+                width={''} 
+                height={''} 
+                attr={[]} 
+          />       
+          <Img 
+                src={LogoFooter2} 
+                alt={''} 
+                srcset={''} 
+                className={'w-50'} 
+                width={''} 
+                height={''} 
+                attr={[]} 
+          />       
+          </div>
         <div className="col-12 text-center">
           <h3 className='fw-bold'>
             <Icon variant={'light'} icon={'arrow-repeat'} name={'いってらっしゃい!!'}
