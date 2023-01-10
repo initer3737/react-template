9af23ca57933c2b7ddee34529306ca9287df00c6
puthutwahyuaji <puthutwahyuaji13@gmail.com>
2023-01-10 13:04:42 +0700

    template  updated

---
+++ src/components/routes/index.tsx
@@ -0,0 +1,38 @@
+import React  from "react";
+    import {
+        Routes as Switch, // similiar to switch
+        Route ,
+        Navigate,
+        // useLocation
+        } from 'react-router-dom'
+    import {
+        NotFound,
+        HomePage
+    } from '../pages'
+    import {
+        Header,
+        Footer,
+        Nav,
+        ScrollToTop
+    } from '../assembleComponent'
+
+export default function Routes(){
+    // const location=useLocation();
+    //     const {pathname}=location;
+    //     const path=pathname.split('/');
+
+     return (
+        <>
+                <ScrollToTop/>
+                 <Nav/>
+                 <Header/> 
+                    <Switch>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/404" element={<NotFound/>} />
+                        <Route path='*' element={<Navigate replace to='/404'/>} />
+                    </Switch>
+                    
+                <Footer/>
+        </>
+     )
+}
